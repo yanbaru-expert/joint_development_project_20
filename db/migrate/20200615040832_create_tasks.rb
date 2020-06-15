01a4a3508fe9b3e764804eff8a9de0453279baa9
kura-masahiro <dacanghongsheng@gmail.com>
2020-06-15 22:54:04 +0900

    新規投稿機能を、ストロングパラメータを利用し、実装しました。またマイグレーションファイルを修正しています。

--- db/migrate/20200615040832_create_tasks.rb
+++ db/migrate/20200615040832_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
-      t.string        :name
-      t.integer      :age
+      t.string        :title
+      t.text          :content
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -16,15 +16,8 @@ ActiveRecord::Schema.define(version: 2020_06_15_040832) do
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.integer "age"
+    t.string "title"
+    t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
