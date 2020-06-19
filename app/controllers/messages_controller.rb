d65f08b187678b6e399340a4975f18d88fc4b781
zoguxy <bsaa.stars7661@gmail.com>
2020-06-20 01:07:04 +0900

    詳細表示機能 showを実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,9 +2,13 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
-  
+
   def new
     @message = Message.new
+  end
+
+  def show
+    @message = Message.find(params[:id])
   end
 
   def create
